9265b3964286e1cec1443c9fe4891fe82ce50971
Chispiitaa <jfigueroar1900@alumno.ipn.mx>
2024-06-20 02:49:11 -0600

    admin tutores

--- editar_tutor.php
+++ editar_tutor.php
@@ -42,6 +42,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="recursos/BootStrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/styleSign.css">
+    <link rel="icon" href="recursos/monitor-de-computadora.png" type="image/x-icon">
     <title>Editar Tutor</title>
 </head>
 <body>
